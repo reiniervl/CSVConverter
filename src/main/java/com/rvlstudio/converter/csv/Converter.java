47609cefaca593a5d747353a742e0148b9306be9
Reinier <rvanleussen@outlook.com>
2019-11-28 17:48:21 +0100

    Update Converter.java

--- src/main/java/com/rvlstudio/converter/csv/Converter.java
+++ src/main/java/com/rvlstudio/converter/csv/Converter.java
@@ -2,6 +2,11 @@ package com.rvlstudio.converter.csv;
 
 import java.util.Collection;
 
+/**
+ * The Converter converts an Object or collection of Objects in to CSV
+ * Only the properties that have a public getter are converted. Number
+ * and String are not treated as compound structures.
+ */
 public interface Converter {
 	/**
 	 * Convert a single object in to one record
@@ -20,5 +25,28 @@ public interface Converter {
 	 */
 	<T extends Collection<?>> CSV convert(T collection);
 
+	/**
+	 * An adapter can change the way a certain class is converted in to a
+	 * cell. If a class holds the column name, this can be changed to be the
+	 * actual column name, instead of the name of the class
+	 *
+	 * @param adapter Implents the TypeAdapter interface to return a Cell
+	 * @param <T> Type of the class
+	 * @return To complete the builder pattern, the registration needs to return the converter
+	 */
 	<T> Converter registerTypeAdapter(TypeAdapter<T> adapter);
+
+	/**
+	 * Options are stored in a map to configure the way the Converter works. In
+	 * particular the String output.
+	 * Default options are:
+	 * delimiter -> ;
+	 * headers -> true
+	 * quoated -> true
+	 *
+	 * @param name String of the options name/key
+	 * @param option the value, also a String
+	 * @return To complete the builder pattern, the registration needs to return the converter
+	 */
+	Converter withOption(String name, String option);
 }
